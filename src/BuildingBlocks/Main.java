a5d85111d5675c8287744ddb8f8f6225cbcee00d
Eren Nokta <eren.nkta@gmail.com>
2024-05-16 13:09:57 +0300

    Chapter01 done and review questions are solved
    Sorular çözüldü ve yeni chapter'a geçiş yapılıyor

--- src/BuildingBlocks/Main.java
+++ src/BuildingBlocks/Main.java
@@ -3,6 +3,7 @@ package BuildingBlocks;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static  int deneme = 0;
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -13,5 +14,6 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+        int a=2,b=4,c;
     }
 }
